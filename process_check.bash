652d2fba1a1519bdba7b9a960c41e646f7210a5b
Arun Vignesh M <40812593+arunvignesh15@users.noreply.github.com>
2019-11-11 22:14:50 +0530

    Update process_check.bash

--- process_check.bash
+++ process_check.bash
@@ -25,12 +25,29 @@ do
 	name=$(basename "$proceess")
 	echo $time_process $user $name >> process.txt
   else
-   echo " Process running less than 5 hours"
-   user=`echo $line | awk '{print $2}'`
-   echo "User Name using:" $user
-   proceess=`echo $line | awk '{print $3}'`
+   	echo " Process running less than 5 hours"
+   	user=`echo $line | awk '{print $2}'`
+   	echo "User Name using:" $user
+   	proceess=`echo $line | awk '{print $3}'`
 	#echo $proceess
-   name=$(basename "$proceess") >> lessthan5hrprocess.txt
+   	name=$(basename "$proceess") >> lessthan5hrprocess.txt
   fi  
 done < "$input"
 
+process=`cat process.txt`
+
+if [ -s "$process" ] 
+then
+	BODY="The following flowcharts are running for more than 5hours.
+	$process
+	Kindly take required actions
+	Thanks"
+	TO_ADDRESS="@gmail.com"
+	FROM_ADDRESS="@gmail.com"
+	SUBJECT="Long Running Process"
+
+echo ${BODY}| mail -s "$SUBJECT" -r $FROM_ADDRESS $TO_ADDRESS
+else
+	echo "No Long running Process found."
+        
+fi
